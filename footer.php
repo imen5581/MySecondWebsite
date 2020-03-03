8df50bb1610cfe5b8fa06627c4717c0b024f922e
imen5581 <Chernyahovsky2010@yandex.ru>
2020-03-03 10:44:25 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+        <div class="col-12 col-md">
+            <img class="mb-2" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted">© 2017-2019</small>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Мы выполняем</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Электропроводка домов</a></li>
+                <li><a class="text-muted" href="#">Электропроводка квартир</a></li>
+                <li><a class="text-muted" href="#">Электропроводка дач</a></li>
+                <li><a class="text-muted" href="#">Электропроводка коттеджей</a></li>
+                <li><a class="text-muted" href="#">Электропроводка кафе и ресторанов</a></li>
+                <li><a class="text-muted" href="#">Ремонт электропроводки</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Свяжитесь с нами</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Мы ВКОНТАКТЕ</a></li>
+                <li><a class="text-muted" href="#">Мы в INSTAGRAM</a></li>
+                <li><a class="text-muted" href="#">Звоните:+79207635466</a></li>
+                <li><a class="text-muted" href="#">Email: Chernyahovsky2010@yandex.ru</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>О нас</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Наши работы</a></li>
+                <li><a class="text-muted" href="#">Наши расценки</a></li>
+                <li><a class="text-muted" href="#">Наши гарантии</a></li>
+                <li><a class="text-muted" href="#">Наша команда</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
