2ba99eb41e50b2a59ede1b1685c2df0b16031e23
imen5581 <Chernyahovsky2010@yandex.ru>
2020-03-03 10:42:30 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Мой второй сайт</title>
+    <link rel="stylesheet" href="css\style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<?php
+require_once "header.php";
+
+require_once 'articles.php';
+?>
+
+
+<?php
+require_once "footer.php";
+?>
+</body>
+</html>
