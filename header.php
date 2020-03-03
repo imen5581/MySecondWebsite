bbedf4e89b5abcfeab4e88bc7498749965201dc7
imen5581 <Chernyahovsky2010@yandex.ru>
2020-03-03 10:42:57 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Электромонтаж в Туле</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Наши работы</a>
+        <a class="p-2 text-dark" href="#">Наши расценки</a>
+        <a class="p-2 text-dark" href="#">Наши гарантии</a>
+        <a class="p-2 text-dark" href="about.php">Контакты</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="#">Войти</a>
+</div>
