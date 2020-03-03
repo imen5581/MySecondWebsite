3fcdbab8f6b219555ef2ceb316d97b7c89ee150e
imen5581 <Chernyahovsky2010@yandex.ru>
2020-03-03 10:43:27 +0300

    Create articles.php

---
+++ articles.php
@@ -0,0 +1,104 @@
+<div class="container mt-5">
+
+    <h3 class="mb-5">Наши статьи</h3>
+
+    <div class="d-flex flex-wrap">
+
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Проводка в квартире</h4>
+            </div>
+            <div class="card-body">
+                <img src="img\1.jpg" alt="" class="img-thumbnail">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Монтаж проводки в квартире</li>
+                    <li>Как избежать ошибок</li>
+                    <li>Как не переплотить</li>
+                    <li>Что следует учитывать</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Подробнее</button>
+            </div>
+        </div>
+
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Свет и натяжные потолки</h4>
+            </div>
+            <div class="card-body">
+                <img src="img\2.jpg" alt="" class="img-thumbnail">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>10 users included</li>
+                    <li>2 GB of storage</li>
+                    <li>Email support</li>
+                    <li>Help center access</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Подробнее</button>
+            </div>
+        </div>
+
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Установка щитка</h4>
+            </div>
+            <div class="card-body">
+                <img src="img\3.jpg" alt="" class="img-thumbnail">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>10 users included</li>
+                    <li>2 GB of storage</li>
+                    <li>Email support</li>
+                    <li>Help center access</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Подробнее</button>
+            </div>
+        </div>
+
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Монтаж проводки в кафе</h4>
+            </div>
+            <div class="card-body">
+                <img src="img\4.jpg" alt="" class="img-thumbnail">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>10 users included</li>
+                    <li>2 GB of storage</li>
+                    <li>Email support</li>
+                    <li>Help center access</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Подробнее</button>
+            </div>
+        </div>
+
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Установка розеток</h4>
+            </div>
+            <div class="card-body">
+                <img src="img\5.jpg" alt="" class="img-thumbnail">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>10 users included</li>
+                    <li>2 GB of storage</li>
+                    <li>Email support</li>
+                    <li>Help center access</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Подробнее</button>
+            </div>
+        </div>
+
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Теплый пол</h4>
+            </div>
+            <div class="card-body">
+                <img src="img\6.jpg" alt="" class="img-thumbnail">
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>10 users included</li>
+                    <li>2 GB of storage</li>
+                    <li>Email support</li>
+                    <li>Help center access</li>
+                </ul>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Подробнее</button>
+            </div>
+        </div>
+
+    </div>
+</div>
